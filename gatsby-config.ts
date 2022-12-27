a2260ec783bba1db276ce6b58ec23994b2cab0b8
yuanjiang <yuanjiang@duia.com>
2022-12-27 20:09:38 +0800

    feat:初始化项目

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,14 +1,12 @@
 import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: [],
+	siteMetadata: {
+		siteName: `Using TypeScript`,
+		sourceUrl: `https://github.com/gatsbyjs/gatsby/tree/master/examples/using-typescript`,
+	},
+	graphqlTypegen: true,
+	plugins: [],
 }
 
 export default config
